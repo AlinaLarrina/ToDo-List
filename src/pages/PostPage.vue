eabe666bb7ddef936f72608649451721a67e5278
Alina Larina <alinalarina2579@gmail.com>
2024-03-20 18:05:15 +0300

    Added v-intersection and v-focus directives, as well as mixins

--- src/pages/PostPage.vue
+++ src/pages/PostPage.vue
@@ -4,6 +4,7 @@
     <my-input
         v-model="searchQuery"
         placeholder="Поиск..."
+        v-focus
     />
     <div class="app__btns">
       <my-button
@@ -27,7 +28,7 @@
         v-if="!isPostsLoading"
     />
     <div v-else>Идет загрузка...</div>
-    <div ref="observer" class="observer"></div>
+    <div v-intersection="loadMorePost" class="observer"></div>
     <!--      <div class="page__wraper">-->
     <!--        <div-->
     <!--            v-for="pageNumber in totalPages"-->
@@ -128,17 +129,17 @@ export default {
   mounted() {
     this.fetchPosts();
     console.log(this.$refs.observer);
-    const options = {
-      rootMargin: "0px",
-      threshold: 1.0,
-    };
-    const callback = (entries, observer) => {
-      if (entries[0].isIntersecting && this.page < this.totalPages) {
-        this.loadMorePost()
-      }
-    };
-    const observer = new IntersectionObserver(callback, options);
-    observer.observe(this.$refs.observer);
+    // const options = {
+    //   rootMargin: "0px",
+    //   threshold: 1.0,
+    // };
+    // const callback = (entries, observer) => {
+    //   if (entries[0].isIntersecting && this.page < this.totalPages) {
+    //     this.loadMorePost()
+    //   }
+    // };
+    // const observer = new IntersectionObserver(callback, options);
+    // observer.observe(this.$refs.observer);
   },
   computed: {
     sortedPosts() {
